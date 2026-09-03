83600979350972a237acbd1e9fac71beee084d97
thomas kiljanczyk dev <thomas.kiljanczyk.dev@gmail.com>
2026-09-03 13:30:21 +0200

    chore: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,16 @@ import { crx } from '@crxjs/vite-plugin';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 
-import manifest from './manifest.json';
-import packageJson from './package.json';
+import manifest from './manifest.json' with { type: 'json' };
+import packageJson from './package.json' with { type: 'json' };
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         tanstackRouter({
             autoCodeSplitting: true,
-            routesDirectory: path.join(__dirname, 'src/popup/routes'),
-            generatedRouteTree: path.join(__dirname, 'src/popup/routeTree.gen.ts')
+            routesDirectory: path.join(import.meta.dirname, 'src/popup/routes'),
+            generatedRouteTree: path.join(import.meta.dirname, 'src/popup/routeTree.gen.ts')
         }),
         react(),
         crx({
@@ -25,9 +25,7 @@ export default defineConfig({
         })
     ],
     resolve: {
-        alias: {
-            '@': path.join(__dirname, './src')
-        }
+        tsconfigPaths: true
     },
     build: {
         outDir: 'dist',
